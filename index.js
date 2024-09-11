7e1738694e5acbc1eb6da09005d91be892bc425f
code-sujal <142722314+code-sujal@users.noreply.github.com>
2024-09-11 11:25:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+var content = [
+  { title: 'Delhi' },
+  { title: 'Mumbai' },
+  { title: 'Jaipur' },
+  { title: 'Agra' },
+  { title: 'Goa' },
+  { title: 'Kolkata' },
+  { title: 'Chennai' },
+  { title: 'Bengaluru' },
+  { title: 'Hyderabad' },
+  { title: 'Udaipur' },
+  { title: 'Varanasi' },
+  { title: 'Amritsar' },
+  { title: 'Pune' },
+  { title: 'Ahmedabad' },
+  { title: 'Jodhpur' },
+  { title: 'Mysore' },
+  { title: 'Shimla' },
+  { title: 'Manali' },
+  { title: 'Rishikesh' },
+  { title: 'Leh' },
+  { title: 'Darjeeling' },
+  { title: 'Coorg' },
+  { title: 'Ooty' },
+  { title: 'Kochi' },
+  { title: 'Thiruvananthapuram' },
+  { title: 'Khajuraho' },
+  { title: 'Ranthambore' },
+  { title: 'Hampi' },
+  { title: 'Mahabalipuram' },
+  { title: 'Pondicherry' },
+  { title: 'Gulmarg' },
+  { title: 'Jaisalmer' },
+  { title: 'Ranchi' },
+  { title: 'Bhubaneswar' },
+  { title: 'Madurai' },
+  { title: 'Tirupati' },
+  { title: 'Aurangabad' },
+  { title: 'Gwalior' },
+  { title: 'Bikaner' },
+  { title: 'Alleppey' }
+  // etc
+];
+
+$('.ui.search')
+  .search({
+    source: content
+  })
+;
